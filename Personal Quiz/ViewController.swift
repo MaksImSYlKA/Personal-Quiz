f1a8f121deaee8fcddcef5f3d4081f1c512a42c3
Maxim Sylka <msylka@me.com>
2024-01-19 22:38:07 +0300

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Максим Сылка on 19.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
