b3126419e2b2e73a6b3cfcf49edd2a22a385f579
Maxim Sylka <msylka@me.com>
2024-02-01 23:09:19 +0300

    handle pressed ansser button in first question

--- Personal Quiz/ViewControllers/QuestionsViewController.swift
+++ Personal Quiz/ViewControllers/QuestionsViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class QuestionsViewController: UIViewController {
-
+    // MARK: - IB Outlets
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var questionProgressView: UIProgressView!
     
@@ -25,12 +25,19 @@ final class QuestionsViewController: UIViewController {
     @IBOutlet var rangedLabels: UIStackView!
     @IBOutlet var rangedSlider: UISlider!
     
-    
+    // MARK: - Private Properties
+    private var questionIndex = 0
     private let questions = Question.getQuestion()
+    private var answers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
+    // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
+    // MARK: - IB Actions
     @IBAction func singlequestionButtonPressed(_ sender: UIButton) {
     }
     
@@ -41,3 +48,47 @@ final class QuestionsViewController: UIViewController {
     
 }
 
+// MARK: - Private Methods
+private extension QuestionsViewController {
+    func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView]{
+            stackView?.isHidden = true
+        }
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        questionLabel.text = currentQuestion.title
+        
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        questionProgressView.setProgress(
+            totalProgress,
+            animated: true
+        )
+        
+        showCurrentQuestionAnswers( for: currentQuestion.type)
+    }
+    
+    
+    /// Choice of answewrs category
+    ///
+    /// Displaying answers to a question according to a category
+    ///
+    /// - Parameter type: Specifies the category of response
+    func showCurrentQuestionAnswers(for type: ResponseType) {
+        switch type {
+            case .single: showSingleStackView(with: answers)
+            case .multiple: break
+            case .ranged: break
+        }
+    }
+    
+    func showSingleStackView (with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons,answers){
+            
+        }
+    }
+}
